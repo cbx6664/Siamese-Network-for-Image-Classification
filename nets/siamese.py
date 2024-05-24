33d842b0317c051fc61cde5dda451b1549832d48
cbx <chenbingxu369@gmail.com>
2024-05-24 11:35:50 +0800

    added ResNet50 as the backbone

--- nets/siamese.py
+++ nets/siamese.py
@@ -2,6 +2,7 @@ import keras.backend as K
 from keras.layers import Dense, Input, Lambda
 from keras.models import Model
 
+from nets.resnet import myResNet50
 from nets.vgg import VGG16
 
 
@@ -12,7 +13,8 @@ def siamese(input_shape):
     #------------------------------------------#
     #   定义VGG16模型
     #------------------------------------------#
-    vgg_model = VGG16()
+    # vgg_model = VGG16()
+    resnet_model = myResNet50()
     #------------------------------------------#
     #   输入是两个图片
     #   input_image_1 : 105, 105, 3
@@ -26,8 +28,10 @@ def siamese(input_shape):
     #   encoded_image_1 : 105, 105, 3 -> 4608
     #   encoded_image_2 : 105, 105, 3 -> 4608
     #------------------------------------------#
-    encoded_image_1 = vgg_model.call(input_image_1)
-    encoded_image_2 = vgg_model.call(input_image_2)
+    # encoded_image_1 = vgg_model.call(input_image_1)
+    # encoded_image_2 = vgg_model.call(input_image_2)
+    encoded_image_1 = resnet_model.call(input_image_1)
+    encoded_image_2 = resnet_model.call(input_image_2)
 
     #------------------------------------------#
     #   相减取绝对值，此处算的是l1距离
@@ -37,11 +41,11 @@ def siamese(input_shape):
     #------------------------------------------#
     #   进行两次全连接
     #------------------------------------------#
-    out = Dense(512, activation = 'relu')(l1_distance)
+    out = Dense(512, activation='relu')(l1_distance)
     #------------------------------------------#
     #   利用sigmoid函数将最后的值固定在0-1之间。
     #------------------------------------------#
-    out = Dense(1, activation = 'sigmoid')(out)
+    out = Dense(1, activation='sigmoid')(out)
 
     model = Model([input_image_1, input_image_2], out)
     return model
--- siamese.py
+++ siamese.py
@@ -22,11 +22,11 @@ class Siamese(object):
         #   使用自己训练好的模型进行预测一定要修改model_path
         #   model_path指向logs文件夹下的权值文件
         # -----------------------------------------------------#
-        "model_path": 'logs/#4_local_dataset_summary_224x224/best_epoch_weights.h5',
+        "model_path": r'logs/resnet50_d0_complete/best_epoch_weights.h5',
         # -----------------------------------------------------#
         #   输入图片的大小。
         # -----------------------------------------------------#
-        "input_shape": [224, 224],
+        "input_shape": [200, 200, 3],
         # --------------------------------------------------------------------#
         #   该变量用于控制是否使用letterbox_image对输入图像进行不失真的resize
         #   否则对图像进行CenterCrop
@@ -71,7 +71,7 @@ class Siamese(object):
     # ---------------------------------------------------#
     #   检测图片
     # ---------------------------------------------------#
-    def detect_image(self, image_1, image_2, image_1_name, test_type):
+    def detect_image(self, query_set_folder, support_set_folder, image_1, image_2, query_image_name, support_image_name):
         # ---------------------------------------------------------#
         #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
         # ---------------------------------------------------------#
@@ -103,12 +103,13 @@ class Siamese(object):
         plt.text(-12, -12, 'Similarity:%.3f' % output, ha='center', va='bottom', fontsize=11)
 
         # 定义保存路径和文件名
-        model_path = self.model_path[:-3]
-        dir = test_type
-        sub_dir = utils.file_name_compare.extract_letters(image_1_name[:-4])
+        sup_dir = self.get_log_path()+"_"+os.path.basename(query_set_folder)+"_"+os.path.basename(support_set_folder)[-2:]
+        dir = query_image_name[:-4]
+        sub_dir = support_image_name
         date = datetime.today().strftime('%Y%m%d')
         filename = f"match_{str(uuid.uuid4())}.png"
-        path = os.path.join(model_path, dir, sub_dir, filename[:-4])
+        path = os.path.join(sup_dir, dir, sub_dir, filename[:-4])
+
 
         # 创建目录（如果不存在）
         os.makedirs(os.path.dirname(path), exist_ok=True)
@@ -120,6 +121,7 @@ class Siamese(object):
         return output
 
     def get_log_path(self):
+        # return os.path.join(self.model_path[:-3], os.path.basename(query_set_folder)+"_"+os.path.basename(support_set_folder))
         return self.model_path[:-3]
 
     def close_session(self):
